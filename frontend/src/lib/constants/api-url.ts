232d111a39a22bdbab412604671b4ebe9b735283
0xGabey <4060669+GabrielCartier@users.noreply.github.com>
2025-04-16 18:25:58 +0700

    updated url for live deployment

--- frontend/src/lib/constants/api-url.ts
+++ frontend/src/lib/constants/api-url.ts
@@ -1,5 +1,4 @@
-// TODO Add api url here
 export const API_URL =
   process.env.NODE_ENV === 'development'
     ? 'http://localhost:3000'
-    : 'https://TODO.com/api';
+    : 'https://bork-tools.fly.dev';
