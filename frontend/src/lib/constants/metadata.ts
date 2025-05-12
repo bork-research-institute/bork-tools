459c8a2b2ac426cb6ba1caeff927e3e1a7e4d437
melondotdev <momodolo.cafe@gmail.com>
2025-05-11 18:38:48 -0700

    added favicon

--- frontend/src/lib/constants/metadata.ts
+++ frontend/src/lib/constants/metadata.ts
@@ -4,4 +4,7 @@ export const METADATA: Metadata = {
   title: 'eggsight',
   description:
     'social media and on-chain analysis tools - powered by the bork research institute',
+  icons: {
+    icon: '/assets/eggsight.ico',
+  },
 };
